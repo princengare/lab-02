a48be05777f27d68e414449d7885641a09c31be6
Dylan Ngare Gatua <89433547+princengare@users.noreply.github.com>
2023-05-08 10:55:37 -0700

    Update tcp_client.py

--- tcp_client.py
+++ tcp_client.py
@@ -11,7 +11,31 @@ def main():
     # TODO: Create a socket and connect it to the server at the designated IP and port
     # TODO: Get user input and send it to the server using your TCP socket
     # TODO: Receive a response from the server and close the TCP connection
-    pass
+    
+    # Define the IP address and port of the server
+    SERVER_IP = 'localhost'
+    SERVER_PORT = 8080
+
+    # Create a socket object
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # Connect the socket to the server
+    client_socket.connect((SERVER_IP, SERVER_PORT))
+
+    # Get user input
+    user_input = input("Enter a message: ")
+
+    # Send the user input to the server
+    client_socket.send(user_input.encode())
+
+    # Receive a response from the server
+    response = client_socket.recv(256).decode()
+
+    # Print the response from the server
+    print("Server response: ", response)
+
+    # Close the socket
+    client_socket.close()
 
 
 if __name__ == '__main__':
